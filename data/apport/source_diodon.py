f31f0b444530fcdd4f2d617dd449841f5362cabf
Oliver Sauder <os@esite.ch>
2013-01-13 21:40:55 +0200

    Added license to apport hook

--- data/apport/source_diodon.py
+++ data/apport/source_diodon.py
@@ -1,8 +1,14 @@
 '''
 apport package hook for diodon
 
-Copyright (C) 2010 Diodon Team
+Copyright (C) 2010-2013 Diodon Team
 Author: Oliver Sauder <os@esite.ch>
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.  See http://www.gnu.org/copyleft/gpl.html for
+the full text of the license.
 '''
 
 import apport
